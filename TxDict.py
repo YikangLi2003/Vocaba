82839a567ba30fbf59a50404e30c1e232c04090e
Tom-Li1 <56505901+Tom-Li1@users.noreply.github.com>
2021-03-25 22:28:32 +0800

    add some #

--- TxDict.py
+++ TxDict.py
@@ -5,9 +5,9 @@ import json
 class TxDict:
     def __init__(self, apikey, wordlst):
         self.apikey = apikey
-        self.wordlst = wordlst
+        self.wordlst = wordlst  # 待生成中文释义的单词列表
         self.url = 'http://api.tianapi.com/txapi/enwords/index?key=' + self.apikey + '&word='
-        self.result = {}
+        self.result = {}  # 生成数据的结果 {'word':'definition'}
         self.error_rprt = []
         self.error_info = {
             100: ['内部服务器错误', '出现此错误码请及时反馈或等待修复'],
@@ -35,24 +35,24 @@ class TxDict:
                 print('[→] ' + e)
 
     def generate_error_info(self, resp, word):
-        if resp['code'] == 250:
-            self.result[word] = 'Error250'
-            self.error_rprt.append(word)
+        if resp['code'] == 250:  # 非严重错误
+            self.result[word] = 'Error250'  # 为查询释义失败的单词添加失败标志
+            self.error_rprt.append(word)  # 添加至错误报告 提示出现错误↓ 无返回值
             print('[!]', str((self.wordlst.index(word)) + 1) + '/' + str(len(self.wordlst)), word, 'Error-250')
-        else:
+        else:  # 严重的错误 无法继续进行其他单词释义查询
             print('[×]', str((self.wordlst.index(word)) + 1) + '/' + str(len(self.wordlst)),
                   word, resp['msg'] + ' ' + self.error_info[resp['code']][1])
-            return True
+            return True  # 显示终止时的进度 单词 错误信息 并返回True
 
     def get_definition(self):
         for word in self.wordlst:
             resp = json.loads(urllib.request.urlopen(self.url + word).read().decode())
-            if 'newslist' in resp:
-                self.result[word] = resp['newslist'][0]['content'].split('|')
+            if 'newslist' in resp:  # newslist键在api反馈中表示操作成功
+                self.result[word] = resp['newslist'][0]['content'].split('|')  # 储存结果 显示进度↓
                 print('[√]', str((self.wordlst.index(word)) + 1) + '/' + str(len(self.wordlst)), word)
             else:
-                if self.generate_error_info(resp, word):
-                    break
+                if self.generate_error_info(resp, word):  # 有返回值代表严重错误 无返回值代表错误不影响主程序运行
+                    break  # 无法继续使用API 停止生成释义
 
         return self.result
 
--- main.py
+++ main.py
@@ -6,12 +6,14 @@ from functions import *
 
 
 def main():
+    # 实例化各个对象
     settings = Settings()
     dictation = Dictation()
 
     while True:
-        show_funcs(settings.funcs_info)
-        index = get_func_index(settings.funcs_info)
+        show_funcs(settings.funcs_info)  # 列出支持的操作
+        index = get_func_index(settings.funcs_info)  # 获取用户提供的操作序号
+        # 判断序号 执行对应的操作
         if index == '1':
             dictation_start(dictation, tk, settings)
         elif index == '2':
